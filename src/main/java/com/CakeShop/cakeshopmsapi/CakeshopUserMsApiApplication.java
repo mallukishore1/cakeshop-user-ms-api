ce98f6f9491e37e8e790746e3b98529484b8d393
kishore <you@example.comgit>
2022-05-05 17:24:57 +0530

    add src pom.xml

---
+++ src/main/java/com/CakeShop/cakeshopmsapi/CakeshopUserMsApiApplication.java
@@ -0,0 +1,14 @@
+package com.CakeShop.cakeshopmsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeshopUserMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeshopUserMsApiApplication.class, args);
+		System.out.println("Started...");
+	}
+
+}
---
+++ src/test/java/com/CakeShop/cakeshopmsapi/CakeshopUserMsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CakeShop.cakeshopmsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeshopUserMsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
